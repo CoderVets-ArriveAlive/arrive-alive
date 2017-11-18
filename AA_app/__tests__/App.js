f2ec55e2372d3efc4c4707838488ea4b5539bc85
Brian Hamil <hamil.brian@gmail.com>
2017-11-17 20:50:19 -0600

    this is the last time I'm trying tonight

--- AA_app/__tests__/App.js
+++ AA_app/__tests__/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
+// I've got a lovely bunch of coconuts.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
